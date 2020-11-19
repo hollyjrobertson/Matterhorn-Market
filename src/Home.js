3315b12912bdb450fc73ecb2a65607f9103fd263
Holly <holly.jo.robertson22@gmail.com>
2020-11-19 16:55:16 -0600

    Added Home Component

---
+++ src/Home.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Home.css';
+import home from './home.jpg';
+
+function Home() {
+    return (
+        <div className="home">
+            <div className="home__container">
+
+                <img className="home__image" src={home} alt="background" />
+
+                <div className="home__row">
+                    {/* Product */}
+                    {/* Product */}
+                </div>
+
+                <div className="home__row">
+                    {/* Product */}
+                    {/* Product */}
+                    {/* Product */}
+                </div>
+
+                <div className="home__row">
+                    {/* Product */}
+                </div>
+            </div>           
+        </div>
+    )
+}
+
+export default Home;
