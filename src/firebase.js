f1c40078e1fc263645b285eac824e7e3f61f8786
Holly <holly.jo.robertson22@gmail.com>
2020-11-18 20:50:01 -0600

    Added Header Component

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCo2MEgWaXFYQwxMMFCfxL0ub4t_QcofUc",
+    authDomain: "matterhorn-site.firebaseapp.com",
+    databaseURL: "https://matterhorn-site.firebaseio.com",
+    projectId: "matterhorn-site",
+    storageBucket: "matterhorn-site.appspot.com",
+    messagingSenderId: "350332897136",
+    appId: "1:350332897136:web:0f650014ed9645389f71b5",
+    measurementId: "G-5QCVMEJJNR"
+};
