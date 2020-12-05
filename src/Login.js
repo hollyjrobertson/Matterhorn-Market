69b3e333f35b88de454d966d62d66b4c2f4600f3
Holly <holly.jo.robertson22@gmail.com>
2020-12-05 10:46:27 -0600

    Added Firebase

--- src/Login.js
+++ src/Login.js
@@ -1,38 +1,63 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import './style/Login.css';
 import logo from './images/logo.png';
 import { Link } from "react-router-dom";
 
-export class Login extends Component {
-    render() {
-        return (
-            <div className="login">
-                <Link to="/">
-                    <img src={logo} className="login__logo" alt="logo" />
-                </Link>
-                <div className='login__container'>
-                <h2>Sign-in</h2>
+function Login() {
+    const [email, setEmail] = useState('');
+    const[password, setPassword] = useState('');
 
-                    <form>
-                        <h5>E-mail</h5>
-                        <input type='text'/>
-
-                        <h5>Password</h5>
-                        <input type='password' />
-
-                        <button type='submit' className='login__signInButton'>Sign In</button>
-                    </form>
-
-                    <p>
-                        By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use & Sale. Please
-                        see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.
-                    </p>
-
-                    <button className='login__registerButton'>Create your Amazon Account</button>
-                </div>
-            </div>
-        )
+    const signIn = e => {
+        e.preventDefault();
+        //Login process
     }
+
+    const register = e => {
+        e.preventDefault();
+        //Register process
+    }
+
+    return (
+        <div className="login">
+            <Link to="/">
+                <img src={logo} className="login__logo" alt="logo" />
+            </Link>
+            <div className='login__container'>
+            <h2>Sign-in</h2>
+
+                <form>
+                    <h5>E-mail</h5>
+                    <input
+                        type='text'
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                    />
+
+                    <h5>Password</h5>
+                    <input
+                        type='password'
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                    />
+
+                    <button
+                        type='submit'
+                        onClick={signIn}
+                        className='login__signInButton'>Sign In</button>
+                </form>
+
+                <p>
+                    By signing-in you agree to the Matterhorn Market's Conditions of Use & Sale. Please
+                    see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.
+                </p>
+
+                <button
+                    onClick={register}
+                    className='login__registerButton'>Create your Matterhorn Account</button>
+            </div>
+        </div>
+    )
 }
+
 
 export default Login;
