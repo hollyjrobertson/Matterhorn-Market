2ac8f6aff194dc6bcd71d9f6e8d7cc5165ef8c4d
Holly <holly.jo.robertson22@gmail.com>
2020-12-05 10:16:34 -0600

    Edited Header

--- src/Login.js
+++ src/Login.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
 import './style/Login.css';
 import logo from './images/logo.png';
+import { Link } from "react-router-dom";
 
 export class Login extends Component {
     render() {
         return (
             <div className="login">
-                <img src={logo} className="login__logo" alt="logo"/>
+                <Link to="/">
+                    <img src={logo} className="login__logo" alt="logo" />
+                </Link>
+                <div className='login__container'>
+                <h2>Sign-in</h2>
+
+                    <form>
+                        <h5>E-mail</h5>
+                        <input type='text'/>
+
+                        <h5>Password</h5>
+                        <input type='password' />
+
+                        <button type='submit' className='login__signInButton'>Sign In</button>
+                    </form>
+
+                    <p>
+                        By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use & Sale. Please
+                        see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.
+                    </p>
+
+                    <button className='login__registerButton'>Create your Amazon Account</button>
+                </div>
             </div>
         )
     }
